2a55305851bb146503c3f4f558eb2bd4c77a2177
moinulibr <moinulibr@gmail.com>
2021-02-27 10:39:42 +0600

    deleted all files from hawladar

--- resources/views/backend/layouts/partials/header_notification.blade.php
+++
@@ -1,8 +0,0 @@
-
-                    <div class="dropdown d-inline-block">
-                        <button type="button" class="btn header-item noti-icon waves-effect" id="page-header-notifications-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="icon-sm" data-feather="user"></i>
-                            
-                            {{ Auth::guard('web')->user()->name }}
-                        </button>
-                      </div>
--- resources/views/backend/merchant/layouts/partials/header_notification.blade.php
+++
@@ -1,7 +0,0 @@
-
-                    <div class="dropdown d-inline-block">
-                        <button type="button" class="btn header-item  waves-effect"  aria-haspopup="true" aria-expanded="false">
-                            <i class="icon-sm" data-feather="user"></i>
-                             {{ Auth::guard('merchant')->user()->name }}
-                        </button>  
-                    </div>
